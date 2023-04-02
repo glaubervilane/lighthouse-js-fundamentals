d03a4d4569a3e9ac6cabd2ef69f15e95c6e6273c
Glauber Marques <glauberfmarques@gmail.com>
2023-04-01 21:00:07 -0600

    Challenger odd checker

---
+++ odds.js
@@ -0,0 +1,10 @@
+function isOdd(num) {
+  if (num % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isOdd(3)); // Output: true
+console.log(isOdd(6)); // Output: false
